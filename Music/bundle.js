aa8dde018b54483525a89fcc1a64f4b007e7b607
kazuto-nishimori <knishimori@middlebury.edu>
2021-08-23 15:05:52 +0900

    Added note number capability

--- Music/bundle.js
+++ Music/bundle.js
@@ -4,7 +4,7 @@ var synth = new ABCJS.synth.CreateSynth();
 var abcjsEditor;
 var visualObj
 let notedur = 2000;
-let notes = ['E,','F,','G,','A','B','C','D','E','F','G','a','b','c','d','e','f','g',"a'","b'","c'","d'","e'"]
+let notes = ['E,,','F,,','G,,','A,','B,','C,','D,','E,','F,','G,','A','B','C','D','E','F','G',"a","b","c","d","e"]
 notes = notes.slice(0,10)
 let randomNums = Array.from({length: 1000}, () => Math.floor(Math.random() * notes.length)).join('');
 randomNums = randomNums.replace(/(\d)\1+/g,function(m,c){return c}).split('')
@@ -13,11 +13,8 @@ function enharmonic(L,A,M,not) {
   document.getElementById('message').innerText = not == 1 ? '' : 'Showing the enharmonic '+L+A+M;
   return [L,A,M]
 }
-/*
-function thisnote(i){
-  let note = notes[parseInt(randomNums[i])];
-  let accOnNote = ['^','_',''][Math.floor(Math.random()*3)];
-  let noteName = note.substring(0,1).toUpperCase();
+
+function thisnote(numberOfNotes){
   let sig = [document.getElementById('key').value,document.getElementById('acc').value,document.getElementById('majmin').value]
   sig = sig.join('') == 'B#maj' ? enharmonic('C', '', 'maj') : // B#maj, E#maj, A#maj, D#maj, G#maj, Dbmin, Gbmin, Cbmin, Fbmin
   sig.join('') == 'E#maj' ? enharmonic('F', 'b', 'maj') :
@@ -28,79 +25,51 @@ function thisnote(i){
   sig.join('') == 'Gbmin' ? enharmonic('F', '#', 'min') :
   sig.join('') == 'Cbmin' ? enharmonic('B', '', 'min') :
   sig.join('') == 'Fbmin' ? enharmonic('E', '', 'min') : enharmonic(sig[0],sig[1],sig[2],1)
-
   let accArray = ['F','C','G','D','A','E','B']
   let keyNum = accArray.indexOf(sig[0]) - 1;
   keyNum += sig[1] == '#' ? 7 : sig[1] == 'b' ? -7 : 0;
   keyNum -= sig[2] == 'min' ? 3 : 0
 
-  let alreadyMarked = keyNum > 1 ? accArray.slice(0,keyNum) : accArray.slice(7+keyNum)
-  if (alreadyMarked.indexOf(noteName) != -1) {
-     note = accOnNote == '^' && keyNum > 1 ? note : accOnNote == '' && keyNum > 1 ? '=' + note : accOnNote == '_' && keyNum > 1 ? '_' + note :
-     accOnNote == '_' && keyNum < 1 ? note : accOnNote == '' && keyNum < 1 ? '=' + note : accOnNote == '^' && keyNum < 1 ? '^' + note :
-     accOnNote + note
-  } else {
-    note = accOnNote + note
-  }
-  let frontEndAcc = accOnNote == '^' ? '#' : accOnNote == '_' ? 'b' : ''
-  document.getElementById('note').innerText = noteName + frontEndAcc
 
-  return "X:1\nK:"+sig.join('')+"\nV:V1 clef=bass\n[V:V1]||"+note+",4|\n";
-}
-*/
-
-function thisnote(i){
-  let note = notes[parseInt(randomNums[i])];
-  let noteName = note.substring(0,1).toUpperCase();
-  var frontEndAcc
-
-  let sig = [document.getElementById('key').value,document.getElementById('acc').value,document.getElementById('majmin').value]
-  sig = sig.join('') == 'B#maj' ? enharmonic('C', '', 'maj') : // B#maj, E#maj, A#maj, D#maj, G#maj, Dbmin, Gbmin, Cbmin, Fbmin
-  sig.join('') == 'E#maj' ? enharmonic('F', 'b', 'maj') :
-  sig.join('') == 'A#maj' ? enharmonic('B', 'b', 'maj') :
-  sig.join('') == 'D#maj' ? enharmonic('E', 'b', 'maj') :
-  sig.join('') == 'G#maj' ? enharmonic('A', 'b', 'maj') :
-  sig.join('') == 'Dbmin' ? enharmonic('C', '#', 'min') :
-  sig.join('') == 'Gbmin' ? enharmonic('F', '#', 'min') :
-  sig.join('') == 'Cbmin' ? enharmonic('B', '', 'min') :
-  sig.join('') == 'Fbmin' ? enharmonic('E', '', 'min') : enharmonic(sig[0],sig[1],sig[2],1)
-
-  let accArray = ['F','C','G','D','A','E','B']
-  let keyNum = accArray.indexOf(sig[0]) - 1;
-  keyNum += sig[1] == '#' ? 7 : sig[1] == 'b' ? -7 : 0;
-  keyNum -= sig[2] == 'min' ? 3 : 0
-
-  let accOnNote = [1,-1,0,0,0,0,0,0][Math.floor(Math.random()*8)];
-
-  let alreadyMarked = keyNum > 0 ? accArray.slice(0,keyNum) : keyNum < 0 ? accArray.slice(7+keyNum) : []
-  let sharpOrFlat = alreadyMarked.indexOf(noteName)
-
-  if (sharpOrFlat == -1) { // this note has no sharp or flat
-    note = accOnNote == 1 ? '^' + note : accOnNote == -1 ? '_' + note : note
-    frontEndAcc = accOnNote == 1 ? '#' : accOnNote == -1 ? 'b' : ''
-  } else {
-    if (keyNum > 0) { //this key has sharps
-      note = accOnNote == -1 ? '=' + note : note
-      frontEndAcc = accOnNote == -1 ? ''  : '#'
-    } else if (keyNum < 0) {
-      note = accOnNote == +1 ? '=' + note : note
-      frontEndAcc = accOnNote == +1 ? ''  : 'b'
+  let notesToPlay = ''
+  let notesToShow = ''
+  for (let n = 0; n < numberOfNotes; n++) {
+    let accOnNote = [1,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0][Math.floor(Math.random()*8)];
+    let note = notes[parseInt(Math.floor(Math.random() * notes.length))];
+    let noteName = note.substring(0,1).toUpperCase();
+    var frontEndAcc
+    let alreadyMarked = keyNum > 0 ? accArray.slice(0,keyNum) : keyNum < 0 ? accArray.slice(7+keyNum) : []
+    let sharpOrFlat = alreadyMarked.indexOf(noteName)
+    if (sharpOrFlat == -1) { // this note has no sharp or flat
+      note = accOnNote == 1 ? '^' + note : accOnNote == -1 ? '_' + note : note
+      frontEndAcc = accOnNote == 1 ? '#' : accOnNote == -1 ? 'b' : ''
+    } else {
+      if (keyNum > 0) { //this key has sharps
+        note = accOnNote == -1 ? '=' + note : note
+        frontEndAcc = accOnNote == -1 ? ''  : '#'
+      } else if (keyNum < 0) {
+        note = accOnNote == +1 ? '=' + note : note
+        frontEndAcc = accOnNote == +1 ? ''  : 'b'
+      }
     }
+    notesToPlay = notesToPlay + note
+    notesToShow = notesToShow + ' ' + noteName + frontEndAcc
   }
 
-  document.getElementById('note').innerText = noteName + frontEndAcc
+  document.getElementById('note').innerText = notesToShow.trim()
+  console.log(notesToPlay.trim())
 
-  return "X:1\nK:"+sig.join('')+"\nV:V1 clef=bass\n[V:V1]||"+note+",4|\n";
+  return "X:1\nK:"+sig.join('')+"\nV:V1 clef=bass\n[V:V1]||"+notesToPlay.trim()+"|\n";
 }
 
 window.onload = function () {
   let i = 1
   document.getElementById('main').addEventListener('click', event => {
     i++
-    visualObj = ABCJS.renderAbc("paper", thisnote(i),{scale:5})
+    visualObj = ABCJS.renderAbc("paper", thisnote(parseInt(document.getElementById('numNotes').value)),{scale:5})
     document.getElementById('note').style.visibility = 'hidden'
   });
-  visualObj = ABCJS.renderAbc("paper", thisnote(i),{scale:5});
+  visualObj = ABCJS.renderAbc("paper", thisnote(parseInt(document.getElementById('numNotes').value)),{scale:5});
   document.addEventListener('keypress',  playsound);
   document.getElementById('playbutton').addEventListener('click',playsound);
   function playsound() {
